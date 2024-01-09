76e801de6fce0e203a32591be5dd5fc5b105fa9d
Savas Ceylan <savasceylan@gmail.com>
2024-01-09 21:47:26 +0100

    [tests] re-organize codes

--- pyfinder/run_tests.py
+++ pyfinder/run_tests.py
@@ -2,9 +2,37 @@
 # -*- coding: utf-8 -*-
 
 import unittest
+import logging
+
+class LoggingFormatter(logging.Formatter):
+    """ 
+    Colors logging entries by their level. The code is taken from
+    https://stackoverflow.com/questions/384076/how-can-i-color-python-logging-output
+    """
+    magenta = "\033[95m"
+    reset = "\x1b[0m"
+    format = "%(asctime)s  %(levelname)-8s  %(message)8s"
+    
+    FORMATS = {logging.INFO: magenta + format + reset}
+
+    def format(self, record):
+        log_fmt = self.FORMATS.get(record.levelno)
+        formatter = logging.Formatter(log_fmt, "%Y-%m-%d %H:%M:%S")
+        return formatter.format(record)
 
 if __name__ == '__main__':
     loader = unittest.TestLoader()
-    suite = loader.discover('tests')
+
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    ch = logging.StreamHandler()
+    ch.setFormatter(LoggingFormatter())
+    logger.addHandler(ch)
+
+    logging.newline = True
+    logging.getLogger().info("-"*40)
+    logging.getLogger().info("Running tests for clients and parsers...")
+    logging.getLogger().info("-"*40)
+    suite = loader.discover('tests/clients')
     runner = unittest.TextTestRunner(verbosity=2)
     runner.run(suite)
--- pyfinder/tests/clients/test_rssm_clients.py
+++ pyfinder/tests/clients/test_rssm_clients.py
@@ -3,12 +3,6 @@
 import unittest
 import os
 import sys
-
-module_path = os.path.abspath(__file__)
-one_up = os.path.join(module_path, '..')
-one_up = os.path.abspath(one_up)
-sys.path.append(one_up)
-
 from clients import RRSMShakeMapClient
 from clients import InvalidQueryOption
 
