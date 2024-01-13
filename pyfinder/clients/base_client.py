a1ff17f3695a93880e75f03fc72ab15977346f85
Savas Ceylan <savasceylan@gmail.com>
2024-01-13 03:17:54 +0100

    [general] Decouple calculation methods from the rest

--- pyfinder/clients/base_client.py
+++ pyfinder/clients/base_client.py
@@ -38,17 +38,6 @@ class BaseClient(ABC):
         """ Query the web service. """
         pass
 
-    @staticmethod
-    def percent_g_to_cm_s2(percent_g):
-        """ 
-        Convert percent g to cm/s^2. Needed for the accelerations in
-        the ESM shakemap web service output.
-        """
-        # Standard acceleration due to gravity in cm/s^2
-        g = 980.665  
-        acceleration_cm_s2 = (percent_g * 0.01) * g
-        return acceleration_cm_s2
-
     def set_agency(self, agency):
         """ Set the agency for the web service. """
         self.ws_client.set_agency(agency)
